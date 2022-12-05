79e188e76dc2e8c3cfe108d634e38ddaa9de18af
lawnchaiir <lawnchaiir@users.noreply.github.com>
2022-12-05 08:10:03 -0500

    Redid some parts of day4. I'd gone with a _very_ quick and dirty implementation, which still exists in part, but at least optimized for the simpler case. Might return to this to clean it up again

--- day-four/src/main.rs
+++ day-four/src/main.rs
@@ -17,17 +17,22 @@ fn main() {
         let first_range = create_range_from_string(pair.next().unwrap());
         let second_range = create_range_from_string(pair.next().unwrap());
 
+        let (first_min, first_max) = first_range;
+        let (second_min, second_max) = second_range;
+
+        if (first_min >= second_min && first_max <= second_max)
+            || (second_min >= first_min && second_max <= first_max)
+        {
+            fully_contained_count += 1;
+            overlap_count += 1;
+            continue;
+        }
+
         first_range_set.clear();
         second_range_set.clear();
 
-        first_range_set.extend(first_range);
-        second_range_set.extend(second_range);
-
-        if first_range_set.is_superset(&second_range_set)
-            || first_range_set.is_subset(&second_range_set)
-        {
-            fully_contained_count += 1;
-        }
+        first_range_set.extend(first_min..=first_max);
+        second_range_set.extend(second_min..=second_max);
 
         if first_range_set
             .intersection(&second_range_set)
@@ -42,12 +47,12 @@ fn main() {
     println!("Overlaps={}", overlap_count);
 }
 
-fn create_range_from_string(s: &str) -> std::ops::RangeInclusive<u32> {
+fn create_range_from_string(s: &str) -> (u32, u32) {
     let mut range_spec = s.split_terminator("-");
     let range_start = range_spec.next().unwrap().parse::<u32>().unwrap();
     let range_end = range_spec.next().unwrap().parse::<u32>().unwrap();
 
-    return range_start..=range_end;
+    return (range_start, range_end);
 }
 
 fn load_input() -> String {
