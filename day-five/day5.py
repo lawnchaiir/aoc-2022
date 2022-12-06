0dcbb6f32514f65065f48df6388cc062871d4bd4
lawnchaiir <lawnchaiir@users.noreply.github.com>
2022-12-05 20:35:05 -0500

    More cleanup

--- day-five/day5.py
+++ day-five/day5.py
@@ -1,6 +1,6 @@
-from abc import ABC, abstractmethod
 import copy
 import re
+from abc import ABC, abstractmethod
 
 with open("input.txt") as f:
     input = f.readlines()
@@ -13,6 +13,7 @@ def find_key_index():
             return i - 1
         i += 1
     return -1
+
 
 # abuse the fact that (at least for my test input), there are no multi-digit column keys
 # thus, we only really need to find the char positions of each of the numerical keys, and
@@ -30,9 +31,9 @@ def build_stacks(input, key_index):
         else:
             stack_positions[idx] = int(c)
 
-    stacks = {stack_positions[k]:[] for k in stack_positions}
+    stacks = {stack_positions[k]: [] for k in stack_positions}
 
-    for i in range(key_index -1, -1, -1):
+    for i in range(key_index - 1, -1, -1):
         line = input[i]
         for pos in stack_positions:
             if not line[pos].isspace():
@@ -40,6 +41,7 @@ def build_stacks(input, key_index):
                 stacks[stack].append(line[pos])
 
     return stacks
+
 
 stacks = build_stacks(input, key_index)
 
@@ -53,7 +55,6 @@ class Solver(ABC):
     def __init__(self, stacks):
         self._stacks = copy.deepcopy(stacks)
 
-
     def parse_movement_procedure(self, line):
         movement_procedure = self._MOVEMENT_PROCEDURE_REGEX.findall(line)[0]
 
@@ -63,15 +64,17 @@ class Solver(ABC):
 
         return (move_count, move_source, move_target)
 
-
     def solve(self):
         for move in self.move_instructions:
             move_count, move_source, move_target = self.parse_movement_procedure(move)
             source_stack = self._stacks[move_source]
             target_stack = self._stacks[move_target]
 
-            self.consume_move_instructions(move_count, source_stack, target_stack)
+            self.consume_move_instructions(move_count,
+                                           source_stack,
+                                           target_stack)
 
+    def print_result(self):
         results = []
         for k in self._stacks:
             stack = self._stacks[k]
@@ -92,16 +95,19 @@ class Part1Solver(Solver):
 
 class Part2Solver(Solver):
     def consume_move_instructions(self, move_count, source_stack, target_stack):
-            move_block = source_stack[-move_count:]
-            target_stack.extend(move_block)
+        move_block = source_stack[-move_count:]
+        target_stack.extend(move_block)
 
-            del source_stack[-move_count:]
+        del source_stack[-move_count:]
 
 
-Solver.move_instructions = input[key_index + 2:] # skip the key and blank line after
+# skip the key and blank line after
+Solver.move_instructions = input[key_index + 2:]
 
-part1_solver = Part1Solver(stacks)
-part1_solver.solve()
+solver = Part1Solver(stacks)
+solver.solve()
+solver.print_result()
 
-part2_solver = Part2Solver(stacks)
-part2_solver.solve()
+solver = Part2Solver(stacks)
+solver.solve()
+solver.print_result()
