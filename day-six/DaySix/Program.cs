2c03e63e0890fe01bb3deb3e4ac6a2e31635a48d
lawnchaiir <lawnchaiir@users.noreply.github.com>
2022-12-06 21:04:43 -0500

    Day 6 refined into a solution I'm much happier with. A bit overwrought, but it was fun!

--- day-six/DaySix/Program.cs
+++ day-six/DaySix/Program.cs
@@ -1,8 +1,8 @@
-﻿using System.Text;
+﻿using System.Collections;
 
-string LoadInput(string path)
+static string LoadInput(string path)
 {
-    using FileStream fs = File.OpenRead("input.txt");
+    using FileStream fs = File.OpenRead(path);
     using StreamReader sr = new StreamReader(fs);
     return sr.ReadToEnd();
 }
@@ -18,44 +18,80 @@ Console.WriteLine(solver.StartOfPacketLoc);
 Console.WriteLine(solver.StartOfMessageLoc);
 
 
+class CircularBuffer<T> : IEnumerable<T>
+{
+    public int Count { get; private set; }
+
+    private readonly T[] Buffer;
+    private readonly int Capacity = 0;
+
+    private int Head = 0;
+
+    public CircularBuffer(int capacity)
+    {
+        Buffer = new T[capacity];
+        Capacity = capacity;
+    }
+
+    public void Add(T item)
+    {
+        Buffer[Head] = item;
+        Head = (Head + 1) % Capacity;
+        Count = Math.Min(Count, Capacity);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return ((IEnumerable<T>)Buffer).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return Buffer.GetEnumerator();
+    }
+}
+
+
 class Solver
 {
-    private readonly StringBuilder ReceiveBuffer = new StringBuilder();
-
-    public int CharsProcessed { get => ReceiveBuffer.Length; }
-
+    public int CharsProcessed { get; private set; } = 0;
     public int StartOfPacketLoc { get; private set; } = -1;
-    private bool FoundPacketStart { get => StartOfPacketLoc != -1; }
-
     public int StartOfMessageLoc { get; private set; } = -1;
-    private bool FoundMessageStart { get => StartOfMessageLoc != -1; }
+
+    private readonly CircularBuffer<char> PacketBuffer = new CircularBuffer<char>(4);
+    private readonly CircularBuffer<char> MessageBuffer = new CircularBuffer<char>(14);
+
+    private static bool IsBufferUnique(CircularBuffer<char> buffer)
+    {
+        int bitFlag = 0;
+        foreach (char c in buffer)
+        {
+            int pos = c & 31; // ordinal alphabet position
+            int flag = 1 << pos;
+            if ((bitFlag & flag) != 0)
+            {
+                return false;
+            }
+            bitFlag |= flag;
+        }
+        return true;
+    }
 
     public void ReceiveChar(char input)
     {
-        if (FoundPacketStart && FoundMessageStart)
+        CharsProcessed += 1;
+
+        PacketBuffer.Add(input);
+        MessageBuffer.Add(input);
+
+        if (StartOfPacketLoc == -1 && CharsProcessed >= 4 && IsBufferUnique(PacketBuffer) )
         {
-            return;
+            StartOfPacketLoc = CharsProcessed;
         }
 
-        ReceiveBuffer.Append(input);
-        if (!FoundPacketStart && ReceiveBuffer.Length >= 4)
+        if (StartOfMessageLoc == -1 && CharsProcessed >= 14 && IsBufferUnique(MessageBuffer))
         {
-            string marker = ReceiveBuffer.ToString(ReceiveBuffer.Length - 4, 4);
-            if (marker.Distinct().ToArray().Length == 4)
-            {
-                //Console.WriteLine(marker);
-                StartOfPacketLoc = ReceiveBuffer.Length;
-            }
-        }
-
-        if (!FoundMessageStart && ReceiveBuffer.Length >= 14)
-        {
-            string marker = ReceiveBuffer.ToString(ReceiveBuffer.Length - 14, 14);
-            if (marker.Distinct().ToArray().Length == 14)
-            {
-                //Console.WriteLine(marker);
-                StartOfMessageLoc = ReceiveBuffer.Length;
-            }
+            StartOfMessageLoc = CharsProcessed;
         }
     }
 }
